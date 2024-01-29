8aa012ea3af6801dcc59229f82628d24fa6c5fc3
Piatnytsia Oleksii <pyatnitsa90@gmail.com>
2024-01-29 13:22:08 +0200

    Roman to Integer

---
+++ src/main/java/org/example/easy/romantointeger/Main.java
@@ -0,0 +1,7 @@
+package org.example.easy.romantointeger;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(RomanToInteger.romanToInt("MCMXCIV"));
+    }
+}
